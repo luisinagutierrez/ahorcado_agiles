ba434fd9ded67c188122e8650fe740c7a547f38f
Luca Gini <luca.giniclc@gmail.com>
2025-06-02 20:59:28 -0300

    test validar intento (in)correcto

--- ahorcado.py
+++ ahorcado.py
@@ -1,33 +1,38 @@
+# ahorcado.py
+
 class Ahorcado:
     def __init__(self):
-        self.palabra_vacia = ""
         self.palabra_a_adivinar = ""
         self.letras_usadas = []
-    
+        self.intentos = 7
+        self.letras_adivinadas = []
+        self.intentos_restantes = 7
+
     def validar_letra(self, letra):
-        if letra not in self.palabra_a_adivinar:
-            self.letras_usadas.append(letra)
-            print (f"Letra incorrecta: {letra}")
-            return False
-        else:
-            self.letras_usadas.append(letra)
-            print (f"Letra correcta: {letra}")
-            return True
-        
+
+        return letra in self.palabra_a_adivinar
+
     def validar_palabra(self, palabra):
-        if palabra != self.palabra_a_adivinar:
-            print (f"Palabra incorrecta: {palabra}")
+
+        return palabra == self.palabra_a_adivinar
+
+    def iniciar_juego(self, palabra):
+
+        self.intentos = 7
+        self.intentos_restantes = 7
+        self.letras_adivinadas = []
+        self.letras_usadas = []
+        self.palabra_a_adivinar = palabra
+
+    def intento(self, letra):
+        if letra in self.letras_usadas:
             return False
-        else:
-            print (f"Palabra correcta: {palabra}")
+
+        self.letras_usadas.append(letra)
+
+        if self.validar_letra(letra):
+            self.letras_adivinadas.append(letra)
             return True
-
-    
-    
-
-
-
-
-
-    
-        
+        else:
+            self.intentos_restantes -= 1
+            return False
--- test_ahorcado.py
+++ test_ahorcado.py
@@ -19,10 +19,36 @@ def test_palabra_correcta():
     juego = Ahorcado()
     juego.palabra_a_adivinar = "agiles"
     assert juego.validar_palabra("agiles") == True
+    
+def test_iniciar_juego():
+    juego = Ahorcado()
+    assert juego.intentos == 7
+    assert juego.letras_adivinadas == []
+    
+def test_intento_incorrecto():
+    juego = Ahorcado()
+    juego.palabra_a_adivinar = "agiles"
+    assert juego.intento("t") == False
+    assert juego.letras_adivinadas == []
+    assert juego.letras_usadas == ["t"]
+    assert juego.intentos_restantes == 6  
+    
+def test_intento_correcto():
+    juego = Ahorcado()
+    juego.palabra_a_adivinar = "agiles"
+    assert juego.intento("i") == True
+    assert juego.letras_adivinadas == ["i"]
+    assert juego.letras_usadas == ["i"]
+    assert juego.intentos_restantes == 7  
+
+        
 
 if __name__ == "__main__":
     test_letra_incorrecta()
     test_letra_correcta()
     test_palabra_incorrecta()
     test_palabra_correcta()
+    test_iniciar_juego()
+    test_intento_incorrecto()
+    test_intento_correcto()
     print("Test passed!")
